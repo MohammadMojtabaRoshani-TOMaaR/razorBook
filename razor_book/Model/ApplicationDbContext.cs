a82fed372f1c75d2a80f2c22e66e296bf391e948
Mohammad Mojtaba Roshani-TOMaaR <mohammadmojtabaroshani@outlook.com>
2021-05-16 16:17:08 +0430

    connection string for docker & db model

---
+++ razor_book/Model/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace razor_book.Model
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ razor_book/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace razor_book.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
